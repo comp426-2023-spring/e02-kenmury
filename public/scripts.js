0e438409589ed208a509ab7f5e0e195c903e97df
Kenny Murillo <kenmury@email.unc.edu>
2023-05-04 05:08:55 -0400

    full implementation

--- public/scripts.js
+++ public/scripts.js
@@ -33,3 +33,38 @@ Array.from(gameMode).forEach(mode => {
   });
 
 playButton.addEventListener
+
+async function playGame() {
+    const game = gameSelection.value;
+    const mode = Array.from(gameMode).find(mode => mode.checked)?.value;
+    const playerMove = mode === "opponent" ? move.value : null;
+    const url = `/app/${game}/play/${playerMove ? playerMove : ""}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    if (mode === "random") {
+        result.textContent = `You chose ${data.player}!`
+    } else {
+        result.textContent = `You ${data.result}!`
+    }
+}
+
+playButton.addEventListener("click", () => {
+    playGame();
+  });
+
+function resetGame() {
+  result.textContent = "";
+  const checkedGameMode = Array.from(gameMode).find(mode => mode.checked);
+  if (checkedGameMode) {
+    checkedGameMode.checked = false;
+  }
+  moveSelection.style.display = "none";
+  gameSelection.value = "rps";
+  document.getElementById("random").checked = true;
+  enableMoves();
+}
+
+resetButton.addEventListener("click", () => {
+  resetGame();
+});
