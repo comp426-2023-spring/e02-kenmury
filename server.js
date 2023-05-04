c48870c8fd61a3e9a74293f57069e0d8e6c9d3c5
Kenny Murillo <kenmury@email.unc.edu>
2023-05-04 03:22:08 -0400

    updated server.js

--- server.js
+++ server.js
@@ -102,3 +102,49 @@ process.on('SIGINT', () => {
         }    
     })
 })
+
+
+import {RPS, RPSLS} from './lib/rpsls.js';
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get('/app/', (req, res) => {
+    res.status(200).send('200 OK');
+})
+
+app.get('/app/rps/', (req, res) => {
+    res.status(200).json(RPS());
+})
+
+app.get('/app/rpsls/', (req, res) => {
+    res.status(200).json(RPSLS());
+})
+
+app.post('/app/rps/play/', (req, res) => {
+    res.status(200).json(RPS(req.body.shot));
+})
+
+app.get('/app/rps/play/:shot', (req, res) => {
+    res.status(200).json(RPS(req.params.shot));
+})
+
+app.post('/app/rpsls/play/', (req, res) => {
+    res.status(200).json(RPSLS(req.body.shot));
+})
+
+app.get('/app/rpsls/play/:shot', (req, res) => {
+    res.status(200).json(RPSLS(req.params.shot));
+})
+
+app.get('/app/rps/play/', (req, res) => {
+    res.status(200).json(RPS(req.query.shot));
+})
+
+app.get('/app/rpsls/play/', (req, res) => {
+    res.status(200).json(RPSLS(req.query.shot));
+})
+
+app.use((req, res) => {
+    res.status(404).send('404 NOT FOUND');
+  });
